620ee439638e1b89754c04d6bc98eca9c58c5987
Dev Vaghasiya <Devvaghasiya176@gmail.com>
2026-05-10 16:43:38 +0530

    Update url ngrock to server

--- lib/core/config/api_config.dart
+++ lib/core/config/api_config.dart
@@ -4,8 +4,8 @@ class ApiConfig {
   const ApiConfig._();
 
   // For Android emulator
-  static const String _androidEmulatorBaseUrl = 'https://bd9f-2401-4900-7922-dfb4-39cb-6a46-5f20-57a3.ngrok-free.app/api';
-  // static const String _androidEmulatorBaseUrl = 'https://swbillingemployeebe-production.up.railway.app/api/';
+  // static const String _androidEmulatorBaseUrl = 'https://bd9f-2401-4900-7922-dfb4-39cb-6a46-5f20-57a3.ngrok-free.app/api';
+  static const String _androidEmulatorBaseUrl = 'https://swbillingemployeebe-production.up.railway.app/api/';
 
   // For iOS simulator / macOS / local web
   static const String _localhostBaseUrl = 'http://127.0.0.1:8000/api';
