f2b20168596219bb093be4ef5e478224240193c1
Dev Vaghasiya <Devvaghasiya176@gmail.com>
2026-05-11 23:15:12 +0530

    Update server url

--- lib/core/config/api_config.dart
+++ lib/core/config/api_config.dart
@@ -4,15 +4,16 @@ class ApiConfig {
   const ApiConfig._();
 
   // For Android emulator
-  //static const String _androidEmulatorBaseUrl = 'https://bd9f-2401-4900-7922-dfb4-39cb-6a46-5f20-57a3.ngrok-free.app/api';
+  // static const String _androidEmulatorBaseUrl = 'https://d699-2401-4900-aafb-626f-7491-31fb-efd0-9340.ngrok-free.app/api';
   static const String _androidEmulatorBaseUrl = 'https://swbillingemployeebe-production.up.railway.app/api/';
 
   // For iOS simulator / macOS / local web
-  static const String _localhostBaseUrl = 'http://127.0.0.1:8000/api';
+  static const String _localhostBaseUrl = 'https://swbillingemployeebe-production.up.railway.app/api/';
 
   // For physical phone on same Wi‑Fi as your backend machine.
-  // static const String _physicalDeviceBaseUrl = 'https://swbillingemployeebe-production.up.railway.app/api/';
-  static const String _physicalDeviceBaseUrl = 'https://bd9f-2401-4900-7922-dfb4-39cb-6a46-5f20-57a3.ngrok-free.app/api';
+  // Example: http://192.168.0.10:8000/api
+  static const String _physicalDeviceBaseUrl = 'https://swbillingemployeebe-production.up.railway.app/api/';
+  // static const String _physicalDeviceBaseUrl = 'https://d699-2401-4900-aafb-626f-7491-31fb-efd0-9340.ngrok-free.app/api';
 
   // Set true when running on a real device.
   static const bool usePhysicalDeviceBaseUrl = false;
