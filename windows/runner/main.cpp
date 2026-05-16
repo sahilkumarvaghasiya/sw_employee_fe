3dd20ec8d58e6e5ce75313a50323e4388efa7c75
sahilkumar vaghasiya <116561090+sahil3155@users.noreply.github.com>
2026-05-16 15:59:26 +0530

    Rename application to RetailPilot across multiple platforms

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"sw_billing_employee_fe", origin, size)) {
+  if (!window.Create(L"RetailPilot", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
